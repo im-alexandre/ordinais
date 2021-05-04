444911159dd925f1e89ff7b78582b2f998974dc9
Alexandre Castro <im.alexandre07@gmail.com>
2021-05-04 01:28:39 -0300

    alteração no campo de comparação

--- electre_mor/static/js/habilitar_campos.js
+++ electre_mor/static/js/habilitar_campos.js
@@ -1,10 +1,10 @@
 // Habilitar campos com o Radio de PF e PJ
 var dicionario = {
-    '-2': 'É muito menos importante que',
-    '-1': 'É Menos importante que',
+    '-2': 'É muito mais importante que',
+    '-1': 'É mais importante que',
     '0': 'É Tão importante quanto',
-    '1': 'É Mais importante que',
-    '2': 'É Muito mais importante que',
+    '1': 'É menos importante que',
+    '2': 'É menos mais importante que',
 }
 function muda_valor(elemento) {
     var paragrafo = document.getElementById('paragrafo_' + elemento.id)
