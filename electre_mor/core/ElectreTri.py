5562391651e7e56ef44172bac53f8d5158732354
Alexandre Castro <im.alexandre07@gmail.com>
2021-12-13 14:42:39 -0300

    -

--- electre_mor/core/ElectreTri.py
+++ electre_mor/core/ElectreTri.py
@@ -126,9 +126,10 @@ class ElectreTri():
         cla = cla[cla['relationship'].isin(['x I b', 'x R b', 'x > b'])]
         try:
             index = len(list(cla.index.values))
+            numero_da_classe = 'Class ' + str(qtde_classes - (index))
         except IndexError:
             index = 1
-        numero_da_classe = 'Class ' + str(qtde_classes - (index))
+            numero_da_classe = 'Class ' + str(qtde_classes)
         return numero_da_classe
 
     def otimista(self):
